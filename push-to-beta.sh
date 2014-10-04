18b5fa9ac5b636d1300fdeb11f5d91d188ba3a44
Tomer Cohen <tomerc+github.com@gmail.com>
2014-10-04 15:50:15 +0300

    Sync Aurora to beta and nightly

---
+++ push-to-beta.sh
@@ -0,0 +1,2 @@
+hg out -r default ssh://hg.mozilla.org/releases/l10n/mozilla-beta/he/
+hg push -r default ssh://hg.mozilla.org/releases/l10n/mozilla-beta/he/
---
+++ push-to-nightly.sh
@@ -0,0 +1,2 @@
+hg out -r default ssh://hg.mozilla.org/l10n-central/he/
+hg push -r default ssh://hg.mozilla.org/l10n-central/he/
